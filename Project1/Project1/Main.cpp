751ef937cd928aa79b718043953862df08fec5d7
Brandon Pramann <bpramann1@gmail.com>
2019-02-06 12:41:03 -0600

    Added Validated Input For Rows and Columns
    The input has some output that we will remove at a later date. The input is also not fully validated as right now it is only checking to make sure that the input is an integer but later we will want to make sure that the integer is a proper value.

--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
 using namespace std;
+
+int numberOfRows;
+int numberOfColumns;
+int intValidatedCin();
+
 int main() 
 {
+	cout << "How many rows:";
+	numberOfRows = intValidatedCin();	
+	cout << numberOfRows; // For testing purposes only
+
+	cout << "How many columns:";
+	numberOfColumns = intValidatedCin();
+	cout << numberOfColumns; // For testing purposes only
+
+	return 0;
+}
+
+int intValidatedCin()
+{
+	int returnInt;
+	while (true)
+	{
+		if (cin >> returnInt)
+		{
+			return returnInt;
+		}
+		else
+		{
+			cout << "Enter a number!" << endl;
+			cin.clear();
+			cin.ignore(cin.rdbuf()->in_avail()); // Author: oldrinb // Location: https://stackoverflow.com/questions/12242078/counting-the-number-of-characters-in-standard-input-after-a-cin-has-been-execute
+		}
+	}
+	return -1;
 }
